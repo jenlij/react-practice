0e30646b02506a40554ccfce2f5a64f441b494a8
jenlij <jljohns1216@gmail.com>
2017-09-13 13:28:23 -0400

    adds redux, actions, reducers

---
+++ props/src/actions.js
@@ -0,0 +1,17 @@
+/*
+this is what we want to produce as an action object:
+{
+    type: ADD_MEMBER,
+    name: 'crusher,
+    rank: 'CMO'
+}
+
+*/
+
+const ADD_MEMBER = 'ADD_MEMBER';
+const BEAM_MEMBER = 'BEAM_MEMBER';
+
+export default {
+    ADD_MEMBER,
+    BEAM_MEMBER
+}
---
+++ props/src/reducers.js
@@ -0,0 +1,3 @@
+const crewMembers = (state, action) => {
+    return state;
+}
