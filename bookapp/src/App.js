deb600e65a32567f829b39e6e59b3fa7ca101d84
jenlij <jljohns1216@gmail.com>
2017-09-23 16:30:48 -0400

    map function on book list not working

--- bookapp/src/App.js
+++ bookapp/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
-import SearchBar from './Components/searchbar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './Components/videolist';
-import VideoDetail from './Components/videodetails';
-import _ from 'lodash';
+import BookList from './containers/book_list';
 
 class App extends Component {
     render() {
         return (
             <div>
+            <BookList />
             </div>
             ); 
     }
--- bookapp/src/index.js
+++ bookapp/src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./App";
+import reducers from "./reducers/reducer_books";
+
+ReactDOM.render(
+  <Provider store={ createStore(reducers) }>
+    <App />
+  </Provider>,
+    document.getElementById("root")
+);
