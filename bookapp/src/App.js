4b3738dd5e2d9fb641ca990a50f33b354acf3545
jenlij <jljohns1216@gmail.com>
2017-09-22 23:49:11 -0400

    sets up book app

---
+++ bookapp/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactDOM from 'react-dom';
+import SearchBar from './Components/searchbar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './Components/videolist';
+import VideoDetail from './Components/videodetails';
+import _ from 'lodash';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+            ); 
+    }
+}
+
+
+export default App;
+
+
