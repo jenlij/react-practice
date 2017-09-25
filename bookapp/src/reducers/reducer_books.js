23988e4d2152f9e109d5b2a56f05d32245e030bb
jenlij <jljohns1216@gmail.com>
2017-09-25 10:32:45 -0400

    adds book details

--- bookapp/src/reducers/reducer_books.js
+++ bookapp/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'JavaScript'},
-        {title: 'Python'},
-        {title: 'R'},
-        {title: 'C++'}
+        {title: 'JavaScript', pages: 101},
+        {title: 'Python', pages: 20},
+        {title: 'R', pages: 150},
+        {title: 'C++', pages: 500}
     ];
 }
