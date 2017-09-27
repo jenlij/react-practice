62a5c5acc041827ef9aa391163f5bae478d0056a
jenlij <jljohns1216@gmail.com>
2017-09-27 18:35:30 -0400

    adds weather api and axios

--- weather-forecast/src/index.js
+++ weather-forecast/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provicer} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
